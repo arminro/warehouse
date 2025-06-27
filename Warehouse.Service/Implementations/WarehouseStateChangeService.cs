0beb0d00e630a4450ff70e6a51f81f1472e80821
armin <armin.romhanyi@gmail.com>
2025-06-27 12:25:03 +0200

    fixed small bug with in and out events regarding pagination

--- Warehouse.Service/Implementations/WarehouseStateChangeService.cs
+++ Warehouse.Service/Implementations/WarehouseStateChangeService.cs
@@ -16,16 +16,18 @@ namespace Warehouse.Service.Implementations
 
         public async Task<CollectionDto<WarehouseStateChangeResponseDto>> GetAllIncomingAsync(int pageNumber = 1, int pageSize = 10)
         {
-            var entries = await _repository.GetMultipleReadOnly()
+            var query = _repository.GetMultipleReadOnly()
                 .Include(sc => sc.Component)
                 .Include(sc => sc.ComponentType)
-                .IgnoreQueryFilters()
-                .Where(sc => sc.Direction == StateChangeDirection.In)
+                .IgnoreQueryFilters() // we may reference deleted elements
+                .Where(sc => sc.Direction == StateChangeDirection.In);
+
+            var entries = await query
                 .Paginate(pageNumber, pageSize)
                 .Select(e => _mapper.Map(e))
                 .ToListAsync();
 
-            int total = await _repository.GetMultipleReadOnly()
+            int total = await query
                .CountAsync();
 
             return new CollectionDto<WarehouseStateChangeResponseDto>()
@@ -37,16 +39,18 @@ namespace Warehouse.Service.Implementations
 
         public async Task<CollectionDto<WarehouseStateChangeResponseDto>> GetAllOutgoingAsync(int pageNumber = 1, int pageSize = 10)
         {
-            var entries = await _repository.GetMultipleReadOnly()
+            var query = _repository.GetMultipleReadOnly()
                .Include(sc => sc.Component)
                .Include(sc => sc.ComponentType)
-               .IgnoreQueryFilters() // we are referencing soft deleted elements
-               .Where(sc => sc.Direction == StateChangeDirection.Out)
+               .IgnoreQueryFilters()
+               .Where(sc => sc.Direction == StateChangeDirection.Out);
+
+            var entries = await query
                .Paginate(pageNumber, pageSize)
                .Select(e => _mapper.Map(e))
                .ToListAsync();
 
-            int total = await _repository.GetMultipleReadOnly()
+            int total = await query
               .CountAsync();
 
             return new CollectionDto<WarehouseStateChangeResponseDto>()
